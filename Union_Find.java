725a0e0d085a46515061f10a2f916e1c8b70476d
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-17 22:46:22 -0400

    Update Union_Find.java

--- Union_Find.java
+++ Union_Find.java
@@ -2,10 +2,16 @@
 public class QuickUnionUF
 {
   private int[] father;
+  private int[] size; // size of each cc
+  private int count;  // num of cc;
   public QuickUnionUF(int N)
   {
     father = new int[N];
-    for (int i = 0; i < N; i++) father[i] = i;
+    for (int i = 0; i < N; i++) {
+      father[i] = i;
+      size[i] = 1;
+    }
+    count = n;
   }
 //  public int find(int i)
 //  {
@@ -27,7 +33,9 @@ public class QuickUnionUF
     int root_p = find(p);
     int root_q = find(q);
     if (root_p != root_q) {  //important to check if equal, avoid cycle
-    father[root_p] = root_q;
+      father[root_p] = root_q;
+      count--;
+      size[root_q】 += size[root_p];
   }
   }
 }
