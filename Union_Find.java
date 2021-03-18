ab2332a786d653b89c087bfd05bd1ff75f6a213f
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-17 22:42:54 -0400

    Update Union_Find.java

--- Union_Find.java
+++ Union_Find.java
@@ -1,25 +1,33 @@
 
 public class QuickUnionUF
 {
-  private int[] id;
+  private int[] father;
   public QuickUnionUF(int N)
   {
-    id = new int[N];
-    for (int i = 0; i < N; i++) id[i] = i;
+    father = new int[N];
+    for (int i = 0; i < N; i++) father[i] = i;
   }
-  public int find(int i)
-  {
-    while (i != id[i])
-  {
-      id[i] = id[id[i]];
-      i = id[i];
-  }
-    return i;
-  }
+//  public int find(int i)
+//  {
+//    while (i != id[i])
+//  {
+//      id[i] = id[id[i]];
+//      i = id[i];
+//  }
+//    return i;
+//  }
+    public int find(int x) {
+        if (x == father[x])  return x;
+        return father[x] = find[father[x]]; 
+    }
+  
+  
   public void union(int p, int q)
   {
-    int i = find(p);
-    int j = find(q);
-    id[i] = j;
+    int root_p = find(p);
+    int root_q = find(q);
+    if (root_p != root_q) {  //important to check if equal, avoid cycle
+    father[root_p] = root_q;
+  }
   }
 }
