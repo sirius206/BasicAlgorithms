6962dd2e7e7069bf63648c39400f83ec46205f18
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-03-20 22:05:30 -0400

    Update InitializeGragh.java

--- InitializeGragh.java
+++ InitializeGragh.java
@@ -1,3 +1,5 @@
+
+//1. to hashmap
 private Map<Integer, Set<Integer>> initializeGraph(int n, int[][] edges){
     Map<Integer, Set<Integer>> graph = new HashMap<>();
     for (int i = 0; i < n; i++) {
@@ -10,4 +12,17 @@ private Map<Integer, Set<Integer>> initializeGraph(int n, int[][] edges){
         graph.get(v).add(u);
     }
     return graph;
+}
+
+//2. to adjacency list
+// Create a new list of lists.
+List<List<Integer>> adjacencyList = new ArrayList<>();
+// Initialise an empty list for each node.
+for (int i = 0; i < n; i++) {
+    adjacencyList.add(new ArrayList<>());
+}
+// Go through the edge list, populating the adjacency list.
+for (int[] edge : edges) {
+    adjacencyList.get(edge[0]).add(edge[1]);
+    adjacencyList.get(edge[1]).add(edge[0]);
 }
