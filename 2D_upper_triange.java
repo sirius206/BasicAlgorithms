713902df2fca5e4284ac69284299d306e6255ebf
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-05-22 21:54:25 -0400

    Create 2D_upper_triange.java

---
+++ 2D_upper_triange.java
@@ -0,0 +1,14 @@
+//1  1  1  1
+//   1  1  1
+//      1  1
+//         1
+  
+//Bottom up
+
+for (int i = len - 1; i >= 0; i--) {
+    for (int dist = 1; dist < len - i; dist++) { }}
+  
+  
+// or
+for (int i = len - 1; i >= 0; i--) {
+    for (int j = i; j < len; j++) {  }}
