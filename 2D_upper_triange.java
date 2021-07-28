bedb76504ba523f6ca8cd5db12286b8a28d4445d
sirius206 <46640143+sirius206@users.noreply.github.com>
2021-07-28 15:26:35 -0400

    Update 2D_upper_triange.java

--- 2D_upper_triange.java
+++ 2D_upper_triange.java
@@ -4,11 +4,14 @@
 //         1
   
 //Bottom up
-
-for (int i = len - 1; i >= 0; i--) {
-    for (int dist = 1; dist < len - i; dist++) { }}
-  
-  
-// or
 for (int i = len - 1; i >= 0; i--) {
     for (int j = i; j < len; j++) {  }}
+
+// or
+for (int i = len - 1; i >= 0; i--) {
+    for (int dist = 1; dist < len - i; dist++) { }}
+
+//对角线
+for (int i = 0; i < n; i++) dp[i][i] = p[i]; //initialize diagonal first
+for (int d = 1; d < n; d++)
+    for (int i = 0; i < n - d; i++){
